1447c64b3f19658e1af609385cc082fc46cce3c2
kzubatov <zubatovks@mail.ru>
2024-04-03 19:27:13 +0300

    my etna fork

--- clone_dependencies.bat
+++ clone_dependencies.bat
@@ -1,4 +1,4 @@
 cd external
 git clone --depth 1 https://github.com/ocornut/imgui.git
 git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
-git clone --depth 1 https://github.com/AlexandrShcherbakov/etna.git
+git clone --depth 1 https://github.com/kzubatov/etna.git -b render_target_state_barriers_fix
